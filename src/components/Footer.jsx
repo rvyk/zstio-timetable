7f550789c023c6708644918ab8caa1892bdca161
rvyk <szymanskipawel327@gmail.com>
2023-09-14 21:35:00 +0200

    Added sponsor brand

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,55 @@
+import { useTheme } from "next-themes";
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 function Footer() {
+  const { theme, resolvedTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
-    <footer className="rounded-lg shadow m-4 w-[90%] sm:w-auto dark:bg-gray-800 transition-all bg-[#2B161B]">
-      <div className="max-w-screen mx-auto p-4 transition-all">
-        <span className="text-sm text-[#ffffff] block text-center dark:text-gray-400">
-          © {new Date().getFullYear()}
-          <span className="ml-1 tracking-wide">
-            Made with ❤️ for ZSTiO by Szymański Paweł & Majcher Kacper
-            <br />
-            <Link
-              target="_blank"
-              href={"https://github.com/rvyk/zstio-timetable/"}
-            >
-              GitHub (GPLv3)
-            </Link>
+    <>
+      {resolvedTheme !== undefined && (
+        <div className="-mb-2">
+          {theme == "light" ? (
+            <img
+              src={"/skill-light.png"}
+              className="h-24"
+              alt="skill-host-logo"
+            />
+          ) : (
+            <img
+              src={"/skill-dark.png"}
+              className="h-24"
+              alt="skill-host-logo"
+            />
+          )}
+        </div>
+      )}
+      <footer className="rounded-lg shadow m-4 w-[90%] sm:w-auto dark:bg-gray-800 transition-all bg-[#2B161B]">
+        <div className="max-w-screen mx-auto p-4 transition-all">
+          <span className="text-sm text-[#ffffff] block text-center dark:text-gray-400">
+            © {new Date().getFullYear()}
+            <span className="ml-1 tracking-wide">
+              Made with ❤️ for ZSTiO by Szymański Paweł & Majcher Kacper
+              <br />
+              <Link
+                target="_blank"
+                href={"https://github.com/rvyk/zstio-timetable/"}
+              >
+                GitHub (GPLv3)
+              </Link>
+            </span>
           </span>
-        </span>
-      </div>
-    </footer>
+        </div>
+      </footer>
+    </>
   );
 }
 
