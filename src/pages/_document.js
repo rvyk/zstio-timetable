cec2f7566707940d3ad1bbcc225b0a506fb654bd
Paweł Szymański <szymanskipawel327@gmail.com>
2023-09-21 13:52:34 +0200

    Changed default language v2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pl">
       <Head>
         <link
           rel="icon"
