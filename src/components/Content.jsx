4387f097cc90863ede57234d6ebd2cd776194247
rvyk <szymanskipawel327@gmail.com>
2023-10-15 18:00:10 +0200

    Small styling things

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -216,7 +216,7 @@ function Content(props) {
       {typeof hours == "object" ? (
         <div
           id="timetable"
-          className="relative overflow-x-auto shadow-md sm:rounded-xl w-[90%] transition-all duration-100"
+          className="relative overflow-x-auto shadow-md md:rounded-xl w-[90%] transition-all duration-100"
         >
           <table className="w-full text-sm text-left transition-all duration-200 text-gray-500 dark:text-gray-400">
             <caption className="p-5 transition-all text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
