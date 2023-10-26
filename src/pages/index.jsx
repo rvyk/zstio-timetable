407b48497bbbb3d16d4eaceda08c3d006113c48f
rvyk <szymanskipawel327@gmail.com>
2023-10-26 22:30:59 +0200

    Help buttons on stuck

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,7 +29,7 @@ export default function Home() {
         <meta property="og:title" content="Plan lekcji ZSTiO" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="min-h-screen w-screen overflow-hidden flex flex-col justify-center items-center lg:bg-[#F7F3F5] bg-[#fff] dark:bg-[#202020] lg:dark:bg-[#171717] transition-all">
+      <div className="min-h-screen w-screen overflow-hidden flex flex-col justify-center items-center bg-[#F7F3F5] dark:bg-[#171717] transition-all">
         <Link href={"https://zstiojar.edu.pl"} className="block md:hidden">
           <img alt="logo" className="w-20 h-20" src={"/icon-192x192.png"} />
         </Link>
@@ -40,12 +40,45 @@ export default function Home() {
           }`}
         >
           <p className="text-xl">Utknąłeś?</p>
-          <Link
-            href="/class/1"
-            className="border-b dark:border-red-400 border-gray-300 px-4"
+
+          <div>
+            <Link
+              href="/class/1"
+              className="text-[#a91712] hover:text-white hover:border-transparent bg-transparent dark:border-[2px] border-[1px] border-[#a91712] mx-2 my-2 hover:bg-[#73110e] transition-all focus:ring-4 focus:outline-none focus:ring-transparent font-medium text-sm px-4 py-2.5 text-center inline-flex items-center dark:text-gray-300 hover:dark:text-white dark:bg-[#202020] dark:rounded-lg dark:border-none dark:hover:bg-[#141414] dark:outline-none"
+              type="button"
+            >
+              Spróbuj przejść dalej
+            </Link>
+
+            <button
+              className="text-[#a91712] hover:text-white hover:border-transparent bg-transparent dark:border-[2px] border-[1px] border-[#a91712] mx-2 my-2 hover:bg-[#73110e] transition-all focus:ring-4 focus:outline-none focus:ring-transparent font-medium text-sm px-4 py-2.5 text-center inline-flex items-center dark:text-gray-300 hover:dark:text-white dark:bg-[#202020] dark:rounded-lg dark:border-none dark:hover:bg-[#141414] dark:outline-none"
+              type="button"
+              onClick={() => {
+                window?.location?.reload(true);
+              }}
+            >
+              Odśwież stronę
+            </button>
+          </div>
+
+          <button
+            className="text-[#a91712] hover:text-white hover:border-transparent bg-transparent dark:border-[2px] border-[1px] border-[#a91712] mx-2 my-2 hover:bg-[#73110e] transition-all focus:ring-4 focus:outline-none focus:ring-transparent font-medium text-sm px-4 py-2.5 text-center inline-flex items-center dark:text-gray-300 hover:dark:text-white dark:bg-[#202020] dark:rounded-lg dark:border-none dark:hover:bg-[#141414] dark:outline-none"
+            type="button"
+            onClick={() => {
+              document.cookie.split(";").forEach(function (c) {
+                document.cookie = c
+                  .replace(/^ +/, "")
+                  .replace(
+                    /=.*/,
+                    "=;expires=" + new Date().toUTCString() + ";path=/"
+                  );
+              });
+
+              window.location.reload(true);
+            }}
           >
-            Kliknij tutaj, aby przejść dalej
-          </Link>
+            Spróbuj wyczyścić pliki cookies
+          </button>
         </div>
       </div>
     </>
