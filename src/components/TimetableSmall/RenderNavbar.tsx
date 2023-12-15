1d3df1e67f5ae0348ffca8c99e009756afc8f24f
majusss <majchermajus@proton.me>
2023-12-15 20:08:01 +0100

    Add inTimetable prop to Navbar component

--- src/components/TimetableSmall/RenderNavbar.tsx
+++ src/components/TimetableSmall/RenderNavbar.tsx
@@ -18,6 +18,7 @@ function RenderNavbar({
       </div>
 
       <Navbar
+        inTimetable={true}
         searchDialog={searchDialog}
         setSearchDialog={setSearchDialog}
         isSnowing={false}
