963b2ac33753b909253f3cea71d82b6c0f2162d9
Kacper Majcher <majchermajus@proton.me>
2024-01-28 22:54:13 +0100

    feat: noscript
    mklkj <3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -52,6 +52,21 @@ const Document = () => {
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body className="flex min-h-screen w-full flex-col justify-center !bg-[#F7F3F5] dark:!bg-[#171717]">
+        <noscript>
+          <div className="flex h-screen w-screen flex-col items-center justify-center bg-white text-center text-black">
+            <h1 className="text-4xl font-bold">Włącz JavaScript!</h1>
+            <p className="text-xl">
+              Ta strona nie działa bez włączonego JavaScriptu.
+            </p>
+            <a href={process.env.NEXT_PUBLIC_TIMETABLE_URL}>
+              Przejdź do źródła
+            </a>
+          </div>
+          <meta
+            http-equiv="refresh"
+            content={`5;url=${process.env.NEXT_PUBLIC_TIMETABLE_URL}`}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
