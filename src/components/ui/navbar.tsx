18ec01b7a690ccdc6eb23700472ef97463f91da9
Filip Piękoś <mail@filippiekos.com>
2024-03-14 20:49:09 +0100

    Add priority prop for logos

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -35,6 +35,7 @@ const Navbar: React.FC = () => {
               layout="fill"
               objectFit="cover"
               objectPosition="center"
+              priority
             />
           </Link>
         </div>
