2e024c9095c233c65970dd82485295f391d1f37d
Kacper Majcher <majchermajus@proton.me>
2024-03-18 18:04:37 +0100

    feat: changed table cell rendering

--- components/main-table.tsx
+++ components/main-table.tsx
@@ -5,7 +5,7 @@ import { TimetableContext } from "@/components/providers/timetable-provider";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import useMediaQuery from "@/lib/mediaquery-hook";
-import { days } from "@/lib/utils";
+import { cn, days } from "@/lib/utils";
 import Link from "next/link";
 import { Fragment, useContext } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
@@ -67,17 +67,29 @@ export function MainTable() {
               <TableHeader>
                 <tr className="h-8">
                   <th></th> {/*DO NOT REMOVE*/}
-                  {days.map((day) => {
-                    const currentDate = new Date();
-                    const nextDay = new Date(currentDate);
-                    nextDay.setDate(currentDate.getDate() + day.index);
+                  {table.dayNames.map((day, index) => {
+                    const today = new Date();
+                    let lastMonday = new Date(today);
+
+                    lastMonday.setDate(today.getDate() - ((today.getDay() + 6) % 7));
+
+                    if (today.getDay() === 6 || today.getDay() === 0) {
+                      lastMonday.setDate(lastMonday.getDate() + 7);
+                    }
                     return (
-                      <th key={day.index} className="border-x px-8 last:border-r-0">
+                      <th key={index} className="border-x px-8 last:border-r-0">
                         <div className="flex items-center gap-5 text-left">
-                          <div className="text-xl font-bold">{nextDay.getDate()}</div>
+                          <div
+                            className={cn(
+                              "text-xl font-bold",
+                              lastMonday.getDate() + index === today.getDate() && "rounded-xl bg-accent px-3 py-2 ",
+                            )}
+                          >
+                            {(lastMonday.getDate() + index).toString().padStart(2, "0")}
+                          </div>
                           <div className="grid">
-                            <span className="capitalize">{day.long}</span>
-                            <span className="line-clamp-1 font-normal text-muted-foreground">Dzień kebsa</span>
+                            <span className="capitalize">{day}</span>
+                            <span className="line-clamp-1 font-normal text-muted-foreground">{"todo"}</span>
                           </div>
                         </div>
                       </th>
@@ -96,47 +108,48 @@ export function MainTable() {
                     </TableHead>
                     {table.lessons.map((day, dayIndex) => {
                       const lessonCell = day[lessonIndex];
-                      if (lessonCell.length > 1) {
-                        if (lessonCell.length === 2) {
-                          return (
-                            <TableCell key={`${dayIndex}-${lessonIndex}`} className="gap-2">
-                              {lessonCell.map((subLesson, subLessonindex) => (
+                      if (lessonCell.length === 1)
+                        return (
+                          <TableCell key={`${dayIndex}-${lessonIndex}`} className="!px-6 !py-7">
+                            <div
+                              className="flex items-center justify-between gap-5 text-nowrap"
+                              key={`${dayIndex}-${lessonIndex}-0`}
+                            >
+                              <span className="grid text-left">
+                                <span className="font-medium">{lessonCell[0].subject}</span>
+                                <span className="text-muted-foreground">{lessonCell[0].teacher}</span>
+                              </span>
+                              <span className="text-xl font-semibold">{lessonCell[0].room}</span>
+                            </div>
+                          </TableCell>
+                        );
+                      if (lessonCell.length === 2)
+                        return (
+                          <TableCell key={`${dayIndex}-${lessonIndex}`} className="!px-6 !py-7">
+                            <div className="flex flex-row justify-between gap-10">
+                              {lessonCell.map((subLesson, subLessonIndex) => (
                                 <div
-                                  className="flex items-center justify-between gap-5 text-nowrap"
-                                  key={subLessonindex}
+                                  className="flex items-center justify-between text-nowrap"
+                                  key={`${dayIndex}-${lessonIndex}-${subLessonIndex}`}
                                 >
-                                  <span className="grid text-left">
-                                    <span className="font-medium">{subLesson?.subject}</span>
-                                    <div className="flex justify-between">
-                                      <span className="text-muted-foreground">{subLesson?.teacher}</span>
-                                      <span className="text-muted-foreground">{subLesson?.room}</span>
-                                    </div>
-                                  </span>
+                                  <div className="inline-flex items-center justify-center gap-1">
+                                    <span className="text-xl font-semibold">
+                                      {subLesson.groupName?.split("/")[0] ?? subLesson.groupName}
+                                    </span>
+                                    <span className="flex flex-col text-left">
+                                      <span className="font-medium">{subLesson.subject}</span>
+                                      <div className="flex justify-between">
+                                        <span className="text-muted-foreground">{subLesson.teacher}</span>
+                                        <span className="text-muted-foreground">{subLesson.room}</span>
+                                      </div>
+                                    </span>
+                                  </div>
                                 </div>
                               ))}
-                            </TableCell>
-                          );
-                        }
-                        return (
-                          <TableCell key={`${dayIndex}-${lessonIndex}`}>
-                            <div className="flex max-w-full items-center justify-between gap-5 text-nowrap">
-                              more than 2 groups not supported yet
                             </div>
                           </TableCell>
                         );
-                      } else {
-                        return (
-                          <TableCell key={`${dayIndex}-${lessonIndex}`}>
-                            <div className="flex items-center justify-between gap-5 text-nowrap">
-                              <span className="grid text-left">
-                                <span className="font-semibold">{lessonCell[0]?.subject}</span>
-                                <span className="text-muted-foreground">{lessonCell[0]?.teacher}</span>
-                              </span>
-                              <span className="text-xl font-medium text-muted-foreground">{lessonCell[0]?.room}</span>
-                            </div>
-                          </TableCell>
-                        );
-                      }
+                      return <TableCell key={`${dayIndex}-${lessonIndex}`}></TableCell>;
                     })}
                   </TableRow>
                 ))}
