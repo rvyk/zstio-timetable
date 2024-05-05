479e22c049a4e9d156eced5a16c628777e88afd4
Kacper Majcher <majchermajus@proton.me>
2024-05-05 12:21:21 +0200

    test 3

--- src/app/substitutions/page.tsx
+++ src/app/substitutions/page.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 
 const SubstitutionRoute: NextPage = async () => {
-  return <>kutas</>;
+  return <>przerwa techniczna</>;
 };
 
 export default SubstitutionRoute;
