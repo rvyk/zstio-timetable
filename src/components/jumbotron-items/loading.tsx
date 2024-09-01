5b4f0c4216d15870cba18ac5f70c30d29de95ea9
rvyk <szymanskipawel327@gmail.com>
2024-09-01 14:42:26 +0200

    style: smaller font size for phones

--- src/components/jumbotron-items/loading.tsx
+++ src/components/jumbotron-items/loading.tsx
@@ -6,10 +6,12 @@ const Loading: React.FC = () => {
   return (
     <div
       role="status"
-      className="mb-4 mt-2 flex w-full items-center justify-center text-xl text-gray-500 transition-all dark:text-gray-300 max-lg:mb-6"
+      className="mb-4 mt-2 flex w-full items-center justify-center text-gray-500 transition-all dark:text-gray-300 max-lg:mb-6"
     >
       <ReloadIcon className="mr-2 mt-1 h-5 w-5 animate-spin lg:h-6 lg:w-6" />
-      <h1 className="lg:text-xl">Wczytywanie planu, proszę czekać</h1>
+      <h1 className="max-sm:text-lg lg:text-xl">
+        Wczytywanie planu, proszę czekać
+      </h1>
     </div>
   );
 };
