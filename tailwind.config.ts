897e0dd94aee1e0fccd5106d7f7a867d2c9f8e78
rvyk <szymanskipawel327@gmail.com>
2024-09-07 19:19:27 +0200

    feat: shadcn, color schema

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,34 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "rgb(var(--background))",
+        foreground: "rgb(var(--foreground))",
+
+        accent: {
+          DEFAULT: "rgb(var(--accent))",
+          secondary: "rgb(var(--accent-secondary))",
+          table: "rgb(var(--accent-table))",
+        },
+
+        primary: "rgb(var(--primary))",
+        lines: "rgb(var(--lines))",
+        star: "rgb(var(--star))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
