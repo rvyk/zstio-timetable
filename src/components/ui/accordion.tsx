3450f1eb012a88fdae3974d71f8f02da924d53d2
rvyk <szymanskipawel327@gmail.com>
2024-09-13 23:28:58 +0200

    style: removed bottom padding from accordion

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -43,7 +43,7 @@ const AccordionContent = React.forwardRef<
     className="-ml-2 w-[calc(100%+16px)] overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ));
 
