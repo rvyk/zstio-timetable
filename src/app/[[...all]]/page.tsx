2e3085c6d451d71875c1110dd209584b970372d7
rvyk <szymanskipawel327@gmail.com>
2024-09-22 09:43:14 +0200

    fix: force dynamic

--- src/app/[[...all]]/page.tsx
+++ src/app/[[...all]]/page.tsx
@@ -9,6 +9,7 @@ import { fetchOptivumList } from "@/lib/fetchers/optivum-list";
 import { fetchOptivumTimetable } from "@/lib/fetchers/optivum-timetable";
 
 export const revalidate = 3600;
+export const dynamic = "force-dynamic";
 
 interface PageParams {
   all?: string[];
