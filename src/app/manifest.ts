49efec324c4ae9870ca0d35ba5780223523ed4a4
Paweł Szymański <szymanskipawel327@gmail.com>
2024-10-24 10:31:42 +0200

    chore: updated manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,14 +3,14 @@ import { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Plan lekcji ZSTiO",
-    short_name: "ZSTiO",
+    short_name: "Plan lekcji ZSTiO",
     description:
       "W prosty sposób sprawdź plan zajęć i zastępstwa dla różnych klas, nauczycieli oraz sal w Zespole Szkół Technicznych i Ogólnokształcących im. Stefana Banacha w Jarosławiu.",
     start_url: "/",
     display: "standalone",
     orientation: "portrait",
-    background_color: "#F22121",
-    theme_color: "#F22121",
+    background_color: "#EF0933",
+    theme_color: "#EF0933",
     icons: [
       {
         src: "/icon-512x512.png",
